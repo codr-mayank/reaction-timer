183105369952d10f3c5e5fb32c6d3a4f572a6425
codr-mayank <44097941+codr-mayank@users.noreply.github.com>
2022-06-08 20:28:27 +0530

    reaction-timer app

---
+++ src/components/Results.vue
@@ -0,0 +1,35 @@
+<template>
+  <p>Reaction Time: {{ score }} ms</p>
+  <p class="remark">Remarks: {{ remark }}</p>
+</template>
+
+<script>
+export default {
+  name: 'Results',
+  props: {
+    score: Number
+  },
+  data() {
+    return {
+      remark: null
+    }
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.remark = 'Ninja Fingers';
+    } else if (this.score < 400) {
+      this.remark = 'Rapid Reflexes';
+    } else {
+      this.remark = 'Snail pace ... :(';
+    }
+  }
+}
+</script>
+
+<style>
+  .remark {
+    font-size: 1.4em;
+    color: #0faf87;
+    font-weight: bold;
+  }
+</style>
